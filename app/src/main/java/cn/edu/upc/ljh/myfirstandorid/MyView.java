b4863da18e06e85f0b14df80b52ea37e2dd62f31
ljhang521 <ljhang521@sina.com>
2020-05-16 06:37:32 +0800

    ljh

---
+++ app/src/main/java/cn/edu/upc/ljh/myfirstandorid/MyView.java
@@ -0,0 +1,14 @@
+package cn.edu.upc.ljh.myfirstandorid;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class MyView extends View {
+    public MyView(Context context){
+        super(context);
+    }
+    public  void onDraw(Canvas canvas){
+
+    }
+}
