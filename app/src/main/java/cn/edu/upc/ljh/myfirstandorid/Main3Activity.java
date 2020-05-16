04f792eeba7550cabe1f1eb97ed7f3c93391b85c
ljhang521 <ljhang521@sina.com>
2020-05-16 11:24:10 +0800

    ljh

--- app/src/main/java/cn/edu/upc/ljh/myfirstandorid/Main3Activity.java
+++ app/src/main/java/cn/edu/upc/ljh/myfirstandorid/Main3Activity.java
@@ -3,18 +3,11 @@ package cn.edu.upc.ljh.myfirstandorid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Main3Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main3);
     }
-    @Override
-    protected void onStart(){
-        super.onStart();
-
-    }
-
-
 }
